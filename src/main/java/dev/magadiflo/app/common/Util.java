129e1d8048e19736b0199a8f681908cf51f42e44
Martín Díaz <magadiflo@gmail.com>
2025-04-17 00:11:10 -0500

    Creating Default Subscriber

---
+++ src/main/java/dev/magadiflo/app/common/Util.java
@@ -0,0 +1,34 @@
+package dev.magadiflo.app.common;
+
+import com.github.javafaker.Faker;
+import org.reactivestreams.Subscriber;
+
+import java.time.Duration;
+
+public class Util {
+
+    private static final Faker faker = Faker.instance();
+
+    private Util() {
+    }
+
+    public static <T> Subscriber<T> subscriber() {
+        return new DefaultSubscriber<>("");
+    }
+
+    public static <T> Subscriber<T> subscriber(String name) {
+        return new DefaultSubscriber<>(name);
+    }
+
+    public static Faker faker() {
+        return faker;
+    }
+
+    public static void sleepSeconds(long seconds) {
+        try {
+            Thread.sleep(Duration.ofSeconds(seconds));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
