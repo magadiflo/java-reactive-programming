a4ea0b798adc3593f08c2710c27c5cbd81da872e
Martín Díaz <magadiflo@gmail.com>
2025-05-07 15:45:40 -0500

    Operador `takeWhile()`

--- src/main/java/dev/magadiflo/app/sec04/Lec05TakeOperator.java
+++ src/main/java/dev/magadiflo/app/sec04/Lec05TakeOperator.java
@@ -5,13 +5,21 @@ import reactor.core.publisher.Flux;
 
 public class Lec05TakeOperator {
     public static void main(String[] args) {
-        take();
+        takeWhile();
     }
 
     private static void take() {
         Flux.range(1, 10)
                 .log("take")
                 .take(3)
+                .log("subs")
+                .subscribe(Util.subscriber());
+    }
+
+    private static void takeWhile() {
+        Flux.range(1, 10)
+                .log("take")
+                .takeWhile(i -> i < 3)
                 .log("subs")
                 .subscribe(Util.subscriber());
     }
