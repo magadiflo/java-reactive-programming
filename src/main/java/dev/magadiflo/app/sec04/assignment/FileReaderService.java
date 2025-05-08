81bc6787724adc810c30c77785d99d931af66d35
Martín Díaz <magadiflo@gmail.com>
2025-05-08 10:41:08 -0500

    Tarea asignada

---
+++ src/main/java/dev/magadiflo/app/sec04/assignment/FileReaderService.java
@@ -0,0 +1,9 @@
+package dev.magadiflo.app.sec04.assignment;
+
+import reactor.core.publisher.Flux;
+
+import java.nio.file.Path;
+
+public interface FileReaderService {
+    Flux<String> read(Path path);
+}
