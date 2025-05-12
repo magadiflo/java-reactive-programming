c21823c65de47b52b4a334c4b44f7fe5aa1b7ec0
Martín Díaz <magadiflo@gmail.com>
2025-05-12 18:05:58 -0500

    Tarea asignada

---
+++ src/main/java/dev/magadiflo/app/sec06/Lec06Assignment.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.app.sec06;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Lec06Assignment {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec06Assignment.class);
+
+    public static void main(String[] args) {
+
+    }
+}
