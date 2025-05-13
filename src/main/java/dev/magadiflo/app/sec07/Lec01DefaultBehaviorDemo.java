a83089601c776f230e3241aced17ed45a0ae2438
Martín Díaz <magadiflo@gmail.com>
2025-05-13 12:40:23 -0500

    Publisher/Subscriber - Default Thread - Demo

---
+++ src/main/java/dev/magadiflo/app/sec07/Lec01DefaultBehaviorDemo.java
@@ -0,0 +1,25 @@
+package dev.magadiflo.app.sec07;
+
+import dev.magadiflo.app.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+//De forma predeterminada, el hilo actual está haciendo todo el trabajo.
+public class Lec01DefaultBehaviorDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec01DefaultBehaviorDemo.class);
+
+    public static void main(String[] args) {
+        Flux<Integer> flux = Flux.<Integer>create(fluxSink -> {
+            for (int i = 1; i < 3; i++) {
+                log.info("Generando: {}", i);
+                fluxSink.next(i);
+            }
+            fluxSink.complete();
+        }).doOnNext(value -> log.info("value: {}", value));
+
+        Runnable runnable = () ->flux.subscribe(Util.subscriber("sub1"));
+        Thread.ofPlatform().start(runnable);
+    }
+}
