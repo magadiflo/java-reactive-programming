459773b2b1a7861d5db0a2d084d4b8ddab5079fb
Martín Díaz <magadiflo@gmail.com>
2025-05-26 12:59:52 -0500

    Sink One - emitValue(...) y EmitFailureHandler

--- src/main/java/dev/magadiflo/app/sec12/Lec01SinkOne.java
+++ src/main/java/dev/magadiflo/app/sec12/Lec01SinkOne.java
@@ -1,12 +1,16 @@
 package dev.magadiflo.app.sec12;
 
 import dev.magadiflo.app.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
 
 public class Lec01SinkOne {
+    private static final Logger log = LoggerFactory.getLogger(Lec01SinkOne.class);
+
     public static void main(String[] args) {
-        demo2();
+        demo1();
     }
 
     private static void demo1() {
@@ -26,5 +30,28 @@ public class Lec01SinkOne {
         mono.subscribe(Util.subscriber("mike"));
 
         sink.tryEmitValue("Hola");
+    }
+
+    private static void demo3() {
+        Sinks.One<Object> sink = Sinks.one();
+        Mono<Object> mono = sink.asMono();
+
+        mono.subscribe(Util.subscriber());
+
+        sink.emitValue("Hola", (signalType, emitResult) -> {
+            log.info("Hola");
+            log.info(signalType.name());
+            log.info(emitResult.name());
+
+            return false;
+        });
+
+        sink.emitValue("Buenas", (signalType, emitResult) -> {
+            log.info("Buenas");
+            log.info(signalType.name());
+            log.info(emitResult.name());
+
+            return false;
+        });
     }
 }
