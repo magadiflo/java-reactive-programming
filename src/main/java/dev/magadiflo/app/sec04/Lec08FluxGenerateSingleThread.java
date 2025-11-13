0e0a268f678fd614f623528cf9180e7591a878bc
Martín Díaz <magadiflo@gmail.com>
2025-11-13 00:19:26 -0500

    Refactoriza: Ejecución secuencial y no concurrente con Flux.generate()

--- src/main/java/dev/magadiflo/app/sec04/Lec08FluxGenerateSingleThread.java
+++ src/main/java/dev/magadiflo/app/sec04/Lec08FluxGenerateSingleThread.java
@@ -5,16 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Lec08FluxGenerateSingleThread {
     private static final Logger log = LoggerFactory.getLogger(Lec08FluxGenerateSingleThread.class);
 
     public static void main(String[] args) {
-        incorrectGenerate();
-    }
-
-    private static void correctGenerate() {
         Flux.generate(
                         () -> 1,
                         (state, sink) -> {
@@ -28,29 +22,5 @@ public class Lec08FluxGenerateSingleThread {
                         }
                 )
                 .subscribe(Util.subscriber());
-    }
-
-    private static void incorrectGenerate() {
-        // ⚠️ Estado compartido entre hilos (aquí está el problema)
-        AtomicInteger sharedState = new AtomicInteger(1);
-
-        // La función generadora accede al estado compartido
-        Runnable task = () -> {
-            Flux.generate(synchronousSink -> {
-                        int state = sharedState.getAndIncrement(); // ❌ Múltiples hilos incrementan el mismo contador
-                        log.info("Emitiendo valor : {}", state);
-
-                        synchronousSink.next("Valor -> " + state);
-
-                        if (state >= 5) {
-                            synchronousSink.complete();
-                        }
-                    })
-                    .subscribe(Util.subscriber());
-        };
-
-        for (int i = 0; i < 3; i++) {
-            new Thread(task, "Hilo-" + i).start();
-        }
     }
 }
